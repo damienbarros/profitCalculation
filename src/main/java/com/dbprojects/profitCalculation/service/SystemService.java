490bca1a4c7636eeaa7fb3469418b839e7e42764
damienbarros <damien.barros@outlook.com>
2025-11-04 19:19:55 +0000

    Update SystemService
    Update SystemService.java that processes the application logic

--- src/main/java/com/dbprojects/profitCalculation/service/SystemService.java
+++ src/main/java/com/dbprojects/profitCalculation/service/SystemService.java
@@ -9,21 +9,31 @@ import org.springframework.stereotype.Service;
 public class SystemService {
 
     //FR1 - handle customer income for each shipment
-    public double calculateIncome(Shipment shipment) {
-        double income = 0.0;
-        for (Customer c : shipment.getCustomers()) {
-            income += c.getPaymentAmount();
+    public double calculateTotalIncome(Shipment shipment) {
+        double total = 0.0;
+        for (Customer customer : shipment.getCustomers()) {
+            total += customer.getPaymentAmount();
         }
-        return income;
+        return total;
     }
 
     //FR2 - handle all costs related to service provision
-    public double calculateCosts(Shipment shipment) {
+    public double calculateTotalCosts(Shipment shipment) {
         double cost = 0.0;
-        for (ServiceProvision s : shipment.getServiceProv()) {
-            cost += s.getCostAmount();
+        for (ServiceProvision service : shipment.getServiceProv()) {
+            cost += service.getCostAmount();
         }
         return cost;
+    }
+
+    //FR3 - calculate the total profit or loss for each shipment
+    public double calculateProfitOrLoss(Shipment shipment) {
+        double income = calculateTotalIncome(shipment);
+        double costs = calculateTotalCosts(shipment);
+        double profitOrloss = income - costs;
+
+        shipment.setProfitOrLoss(profitOrloss);
+        return profitOrloss;
     }
 
 }
